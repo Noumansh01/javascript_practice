b892d2f5caf70b867cc6eef237184b741e97fbd9
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-11-19 21:14:38 +0500

    make array and iterate by using for of loop

--- task2.js
+++ task2.js
@@ -6,3 +6,10 @@ for(val of str){
 
     console.log(val);
 }
+
+// write practice question of for of loop by iterating on  array 
+
+let arr= [1,2,"a","b",3,4,"c",5];
+for(arr1 of arr){
+    console.log(arr1);
+}
