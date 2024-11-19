2d3c71ab21f1dad21a0ab39d4178b975a75c29c1
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-11-19 18:03:04 +0500

    change file name

---
+++ task.js
@@ -0,0 +1,17 @@
+// practice question 1
+let product = {
+    tittle: "Ball Pen",
+    rating: 4,  
+    offer: 5,
+    price: 270
+
+};
+console.log(product);
+// practice question 2
+let profile = {
+username: "Nouman",
+isFollow: false,
+followers: 125,
+following: 200
+};
+console.log(profile);
