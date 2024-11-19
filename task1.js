ad7ceaa9cc0204076d3bdd1884857a7e20f63973
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-11-19 21:09:42 +0500

    make new file and write new line of code

--- task1.js
+++ task1.js
@@ -48,5 +48,5 @@ if(score >= 90 && score <= 100){
 let sum = 0;
 for(let i=1; i<=10; i++){
     sum+=i;
-};
+}
 console.log("The total sum is", sum);
---
+++ task2.js
@@ -0,0 +1,8 @@
+// write practice question of for of loop by iterating on a string
+
+let str= "My name is Nouman";
+
+for(val of str){
+
+    console.log(val);
+}
