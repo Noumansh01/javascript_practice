71dbd12e6715dc9498ae697599f2ec9636c28131
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-11-19 18:17:46 +0500

    write new line of code

--- task.js
+++ task.js
@@ -1,4 +1,5 @@
 // practice question 1
+// make object for product
 let product = {
     tittle: "Ball Pen",
     rating: 4,  
@@ -8,6 +9,7 @@ let product = {
 };
 console.log(product);
 // practice question 2
+// make object for profile
 let profile = {
 username: "Nouman",
 isFollow: false,
@@ -15,3 +17,14 @@ followers: 125,
 following: 200
 };
 console.log(profile);
+
+
+// get user to input a number using prompt("Enter a number:"). Check if the number is multiple of  5 or not?
+
+const userInput= prompt("Enter a number:");
+console.log(userInput);
+if(userInput % 5 == 0){
+    console.log(true);
+}else{
+    console.log(false);
+};
