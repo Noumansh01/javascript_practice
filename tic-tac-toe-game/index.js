92afe06f668708e79a2656110042a1c2894d6c87
Noumansh01 <139547809+Noumansh01@users.noreply.github.com>
2024-11-25 20:30:51 +0500

    near to finish code

--- tic-tac-toe-game/index.js
+++ tic-tac-toe-game/index.js
@@ -1,5 +1,9 @@
 let boxes= document.querySelectorAll(".box");
 let btn= document.querySelector("#reset-btn");
+let msg= document.querySelector("#msg");
+let msg_box= document.querySelector(".msg-box");
+let new_btn= document.querySelector("#new-btn");
+
 
 let turn0= true;
 
@@ -15,6 +19,12 @@ const gameWinPattern=[
 
 ]
 
+const reset=()=>{
+    turn0=true;
+    enablebtn();
+    msg_box.style.display="hide";
+}
+
 boxes.forEach((box)=>{
 box.addEventListener("click", ()=>{
     
@@ -26,10 +36,48 @@ box.addEventListener("click", ()=>{
         turn0=true;
     }
         box.disabled=true;
+
+        
+
+        checkWinner();
 })
 })
 
-btn.addEventListener("click",()=>{
-    boxes.innerText="";
-    console.log("click");
-})
+function disablebtn(){
+for(let box of boxes){
+    box.disabled=true;
+}
+}
+function enablebtn(){
+for(let box of boxes){
+    box.disabled=false;
+    box.innerText="";
+}
+}
+
+function showWinner(winner){
+    msg_box.style.display="block";
+    msg.innerText=`${winner} is winner`;
+    disablebtn();
+}
+
+function checkWinner(){
+    for(let pattern of gameWinPattern){
+      console.log( boxes[pattern[1]], boxes[pattern[2]] );
+
+      let pos1=boxes[pattern[0]].innerText;
+      let pos2=boxes[pattern[1]].innerText;
+      let pos3=boxes[pattern[2]].innerText;
+      if(pos1 != "" && pos2 != "" && pos3 != ""){
+        if(pos1 === pos2 && pos2 === pos3){
+            console.log("winner", pos1);
+            showWinner(pos1);
+            
+        }
+      }
+    }
+
+}
+
+btn.addEventListener("click",reset);
+new_btn.addEventListener("click",reset);
